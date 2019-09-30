8932d7c9738e33e27012a5e9fb3a6cbb82cc4ac5
tan-nate <nktan93@gmail.com>
2019-09-30 14:47:04 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+document.getElementById('main').remove()
+
+const newHeader = document.createElement('h1')
+document.body.appendChild(newHeader)
+newHeader.innerHTML = "Nate is the champion"
+newHeader.id = "victory"
