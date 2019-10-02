8e95b0e61fed2f27beafeb4f615ccd3853df9a13
ch2isk4kos <ch2isk4kos@gmail.com>
2019-10-02 19:52:28 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Write your code here!
+// remove main element from index.html programmatically
+document.querySelector('main#main').remove();
+
+// create an h1 tag && assign it to a variable
+let newHeader = document.createElement("h1");
+
+// create a text node && assing it to a variable
+let content = document.createTextNode("YOUR-NAME is the champion");
+
+// assign id=#victory to newHeader
+newHeader.id = "victory";
+
+// add the text node to the h1 tag
+newHeader.appendChild(content);
