07a3a84e4a714e7f60809ef09a6d4653707a77fc
Jonathan Wong <jonathan.wong110@gmail.com>
2019-10-03 06:15:49 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+ // Write your code here!
+ 
+document.body.remove("main#main");
+let newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.innerHTML = "First is the champion";
