11e087acf0b2215b44b0967a2f9396da665cf88b
Jane Venter <janeanneventer@gmail.com>
2019-10-11 00:29:14 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code here!
+
+let m = document.selectById("#main");
+m.remove();
+let newHeader = document.createElement("h1 #victory");
+newHeader.innerHTML = `${YOUR-NAME} is the champion`
