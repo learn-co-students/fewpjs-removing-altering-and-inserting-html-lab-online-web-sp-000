a4dfe55e362cedf3c31246dbbdc80f5037cbfee5
Jeff Benton <sikletz@gmail.com>
2019-11-17 06:42:56 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+main.remove();
+
+var newHeader = document.createElement("h1");
+document.body.appendChild(newHeader);
+newHeader.id = 'victory';
+newHeader.innerHTML = "asdfasdfadsf is the champion!";
