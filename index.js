2257ee1776acf9fecc6d446d66c4bdccfc9914ad
Mark Daniel Delgado <markpnoi26@gmail.com>
2019-11-25 16:47:43 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Write your code here!
+// removes main inside body
+let main = document.getElementById("main")
+main.remove()
+
+
+// has a 'newHeader' variable that points to node 'h1#victory'
+// has a 'newHeader' variable that points to node 'h1#victory'
+// has a 'newHeader' variable that points to node 'h1#victory' with "YOUR-NAME is the champion" inside
+
+let newHeader = document.createElement("h1")
+newHeader.className = "victory"
+newHeader.id = "victory"
+newHeader.innerHTML = "Mark is the champion"
+document.body.appendChild(newHeader)
+
+//
