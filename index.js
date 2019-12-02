8b98c80dee33e15566c63b79f4dbd7a3d29a02ad
Anthony Graniero <ananfro@gmail.com>
2019-12-02 22:40:36 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+var el = document.getElementById('main');
+el.remove();
+
+var newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.innerHTML = 'anthony is the champion';
