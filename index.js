9e7ae58ac978e2d700929c1a2c70d66e1ca585ac
boom100100 <bsd231@nyu.edu>
2019-12-26 11:18:55 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+document.querySelector('main#main').remove();
+let newHeader = document.createElement("H1");
+newHeader.id = "victory";
+
+let newHeaderText = document.createTextNode("YOUR-NAME is the champion");
+newHeader.appendChild(newHeaderText);
