ed1ab2e923aa7eb16c0eeeb50b1aaccecb70d5f7
Lindsey Wells <github email address>
2019-12-30 14:19:09 +0000

    Done.

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ document.querySelector("main#main").remove();
 
 let newHeader = document.createElement('h1');
 newHeader.id = 'victory';
-newHeader.innerHTML = "YOUR-NAME is the champion";
+newHeader.innerHTML = "Lindsey is the champion";
