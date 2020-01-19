de9252ba3ae30d9348e9b7e312cc3dc9e1ed1c86
KJablonski08 <kjablonski08@gmail.com>
2020-01-19 13:02:33 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+
+//no longer has DOM node 'main#main'
+
+document.querySelector('main#main').remove();
+
+//has a 'newHeader' variable that points to node 'h1#victory'
+
+const newHeader = document.createElement('h1') 
+newHeader.id = 'victory'
+newHeader.innerHTML = 'Katie is the champion'
