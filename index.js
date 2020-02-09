ab1cfa1849c6081edb1aa9286997de4e8962bc74
Christian Perez <perezchristian1012@gmail.com>
2020-02-09 03:05:56 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+let main = document.getElementById("main")
+main.remove()
+
+let newHeader = document.createElement("h1")
+newHeader.className = "victory"
+newHeader.id = "victory"
+newHeader.innerHTML = "Christian is the champion"
+document.body.appendChild(newHeader)
