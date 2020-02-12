728fb548da94345e976868e3abde2cd4a2f7a227
Mark <m.hern1415@gmail.com>
2020-02-11 16:19:18 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+
+main.remove()
+
+
+let newHeader = document.createElement('H1#victory');
+
+
+//let newHeader = document.querySelector('H1#victory');
+newHeader.innerHTML = 'Mark is the champion'
+
