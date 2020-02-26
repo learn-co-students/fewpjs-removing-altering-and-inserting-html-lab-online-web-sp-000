9be1193fda8c0dde2c90fcd141e450b9a7987097
rich0024 <yance0022@gmail.com>
2020-02-25 21:32:32 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+document.querySelector("main#main").remove()
+let newHeader = document.createElement("h1")
+document.body.appendChild(newHeader)
+newHeader.id = "victory"
+newHeader.innerHTML = 'Rich is the champion' 
