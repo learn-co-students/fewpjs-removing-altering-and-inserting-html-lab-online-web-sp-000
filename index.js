f29869280c1b5ff5d87b7d464fffb54912fba273
Omar Gonzalez <ogonzaleznyc@gmail.com>
2020-03-03 05:24:19 +0000

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // Write your code here!
-document.querySelector('main').remove();
+//document.querySelector('main').remove();
+document.getElementById('main').remove();
 let newHeader = document.createElement("h1");
 newHeader.id = 'victory';
 newHeader.innerHTML = 'Omar is the champion';
+
