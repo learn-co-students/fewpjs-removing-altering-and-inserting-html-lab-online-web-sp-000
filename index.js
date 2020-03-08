b4b0f671700a814ddf6faa078f77aa782410ff3e
Amy Westcott <amy.westcott@yahoo.com>
2020-03-08 22:46:57 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+document.querySelector('main#main').remove()
+
+//create up the new element h1, give it a CSS id of "victory", and give it innerHTML of what the tests wanted.
+let newHeader = document.createElement('h1')
+newHeader.id = "victory"
+newHeader.innerHTML = "Jeremiah is the champion"
+
+//Actually add the newHeader to the DOM
+document.body.appendChild(newHeader)
