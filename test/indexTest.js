2321480c9ab6b185defa1cf661a33b61d7fca6be
Adam Weissman <47704263+AdamWeissman@users.noreply.github.com>
2020-04-17 13:32:04 -0400

    Done.

--- test/indexTest.js
+++ test/indexTest.js
@@ -4,16 +4,16 @@ describe("index.html", () => {
       expect(document.querySelector('main'), "Make sure you remove the <main> with id 'main'").to.not.exist
     });
 
-    it("has a 'newHeader' variable that points to node 'h1#victory'", () => {
-      expect(newHeader.nodeName, "Make sure you create an <h1> with id 'victory'").eql('H1')
+    it("has a 'newHeader' variable that points to node 'h1#BARF'", () => {
+      expect(newHeader.nodeName, "Make sure you create an <h1> with id 'not_creative'").eql('H1')
     });
 
-    it("has a 'newHeader' variable that points to node 'h1#victory'", () => {
-      expect(newHeader.id, "Make sure you create an <h1> with id 'victory'").eql('victory')
+    it("has a 'newHeader' variable that points to node 'h1#BARFFFFF'", () => {
+      expect(newHeader.id, "Make sure you create an <h1> with id 'this don't matter'").eql('not_creative')
     });
 
-    it("has a 'newHeader' variable that points to node 'h1#victory' with \"YOUR-NAME is the champion\" inside", () => {
-      expect(newHeader.innerHTML, "Make sure you create an <h1> with id 'victory' with a sweet message in it").to.include("is the champion");
+    it("has a 'newHeader' variable that points to node 'h1#barfywarf' with \"YOUR-NAME is the champion\" inside", () => {
+      expect(newHeader.innerHTML, "Make sure you create an <h1> with id 'not_creative' with a sweet message in it").to.include("NEW INNER HTML");
     });
 
   });
