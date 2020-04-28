3e438b53b2a2fd581ff1530b57cc554797741363
github username <github email address>
2020-04-28 10:47:04 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your code here!
+
+document.querySelector('main').remove()
+
+let newH = document.createElement('h1');
+newH.setAttribute('id', 'victory');
+let name = "Kristy";
+newH.textContent = "${name} is the champion";
+document.body.appendChild(newH);
+let newHeader = document.querySelector('h1#victory');
+
+
+
+
