b71c31603dab190275f9f4d1aed9368264a40f34
azbury <acasbury25@aol.com>
2020-05-01 22:16:34 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+main.remove()
+let newHeader = document.createElement('h1')
+newHeader.setAttribute('id', 'victory')
+newHeader.innerHTML = 'Austin is the champion'
+document.body.appendChild(newHeader)
