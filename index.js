b898f6c4e16b31829ca7db33dd281a1b064a5f17
github username <github email address>
2020-05-21 20:10:44 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+
+let main = document.getElementById('main');
+main.remove();
+
+let newHeader = document.createElement('h1');
+newHeader.setAttribute('id', 'victory');
+
+let myJob = document.getElementById('victory');
+newHeader.textContent = 'Lorena is the champion';
+
+myJob.appendChild(newHeader);
