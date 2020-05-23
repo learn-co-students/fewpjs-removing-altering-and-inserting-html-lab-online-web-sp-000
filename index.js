8f4259bd7c2ba6355adabe4f69880c668cd094c2
SLarkworthy <Sarah.Larkworthy@gmail.com>
2020-05-23 14:57:49 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your code here!
+let element = document.querySelector("main");
+element.remove();
+
+let header = document.createElement('header');
+document.body.appendChild(header);
+
+let newHeader = document.createElement('h1');
+newHeader.setAttribute("id", "victory");
+newHeader.innerHTML = 'Sarah is the champion';
+header.appendChild(newHeader);
