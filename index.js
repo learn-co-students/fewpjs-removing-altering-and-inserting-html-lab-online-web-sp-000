e71eb2df573fbeb60888058501538751f8f6094f
nathan-stamand <nathansaint810@gmail.com>
2020-06-15 11:14:42 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+document.querySelector("main#main").remove()
+
+let element = document.createElement('h1')
+element.id = "victory"
+document.body.appendChild(element)
+element.innerHTML = "Nathan is the champion"
+
+let newHeader = document.querySelector("h1")
