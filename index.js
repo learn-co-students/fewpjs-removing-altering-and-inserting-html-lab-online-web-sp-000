534143ae043eaab2209d5238be9e57909a41b4c7
Chad Lengefeld <chadlengefeld@gmail.com>
2020-06-17 09:08:52 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Write your code here!
+// // Write your code here!
+// let element = document.createElement('div')
+// document.body.appendChild(element)
+//
+// let ul = document.createElement('ul')
+//
+//   for (let i = 0; i < 3; i++) {
+//     let li = document.createElement('li')
+//     li.innerHTML = (i + 1).toString()
+//     ul.appendChild(li)
+//   }
+//
+// let element = document.querySelector('p#greeting');
+// element.innerHTML = 'Hello, DOM!';
+// element.style.backgroundColor = '#27647B';
+main.remove()
+
+
+let newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.innerHTML = "Chad is the champion!";
