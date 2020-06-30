9d2730bc368368b5ad6bc15f5b50559c4e1e4f21
felipeldn <felipeldn91@gmail.com>
2020-06-30 18:46:16 +0100

    Done.

--- index.js
+++ index.js
@@ -7,5 +7,5 @@ document.querySelector("main#main").remove();
 /* Create your new element here and assign it to newHeader */
 const newHeader = document.createElement("h1");
 newHeader.id = "victory"
-newHeader.innerHTML = "Flatiron School is the champion!";
+newHeader.innerHTML = "Felipe is the champion!";
 newHeader.className = "victory";
