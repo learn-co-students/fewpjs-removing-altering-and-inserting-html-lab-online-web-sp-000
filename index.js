0f00ecd3dee385d771ec7478fb8af6030b9fa858
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-08-16 22:48:05 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+
+const testVar = "hi"
+
+document.querySelector("main#main").remove();
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory"
+newHeader.innerHTML = "Ryan Tierney is the champion" 
+newHeader.class = "victory"
