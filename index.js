ce01e4c680640a51455dba028acb0fb58bd37ba3
ramseychapin <ramsey@ramseychapin.com>
2020-08-22 11:58:45 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+document.getElementById('main').remove();
+
+var newHeader = document.createElement('h1');
+newHeader.setAttribute("id","victory");
+newHeader.innerHTML = "Ramsey is the champion";
+
+document.body.appendChild(newHeader);
