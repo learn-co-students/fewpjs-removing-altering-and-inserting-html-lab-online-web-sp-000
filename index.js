1dff1ca0f531309f61526ef46d8db09820c97a0d
agiletkiewicz <a.letkiewicz@gmail.com>
2020-08-25 18:31:00 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+
+let main = document.querySelector('#main')
+
+main.remove()
+
+let newHeader = document.createElement('h1')
+
+ newHeader.innerHTML = "YOUR-NAME is the champion"
+ newHeader.id = "victory";
