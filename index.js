a840a255969d0acd5852e605fffb60475ef1f15b
Elliot Blanchard <git@elliotblanchard.com>
2020-08-29 15:26:51 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code here!
+let element = document.getElementById("main");
+element.remove();
+
+let newHeader = document.createElement('h1');
+document.body.appendChild(newHeader);
+newHeader.setAttribute("id", "victory");
+newHeader.innerHTML = `YOUR-NAME is the champion`;
