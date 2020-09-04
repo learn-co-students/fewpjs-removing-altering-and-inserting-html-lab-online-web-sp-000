03bd5ab6ded91e9664f8ba00dbfb3a6df0d49aa2
Emily Gann <aspentree79@gmail.com>
2020-09-04 16:25:17 -0400

    Done.

--- index.js
+++ index.js
@@ -1,17 +1,5 @@
-// Write your code here!
+main.remove();
 
-
-main.remove()
-// should be document.querySelector("main#main").remove();
-
-const newHeader = document.createElement('h1');
-// first create a new element within the document; we are creating an h1 and calling it 'newHeader'
-newHeader.id = "victory";
-// we are assigning the new h1 an id of 'victory'
-newHeader.innerHTML = "Emily is the champion";
-// we are adding text to the h1
-newHeader.className = "victory";
-// we are giving the h1 a class name of 'victory'
-
-// So, newly created h1 element in the document is assigned as a variable called 'newHeader'
-// It is given an id; text is added to the h1; the h1 element is given a class name
+let newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.innerHTML = 'Emily is the champion'
