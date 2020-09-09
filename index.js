02ccb0629a363f5b793110b822ec521c066f43b4
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-09-09 11:34:33 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+// removes main
+main.remove();
+
+// creates newHeader variable
+const newHeader = document.createElement("h1");
+// gives newHeader an id of victory
+newHeader.id="victory"
+newHeader.innerHTML = "Flatiron School is the champion!";
