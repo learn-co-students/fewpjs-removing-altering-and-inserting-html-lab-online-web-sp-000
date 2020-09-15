33a95d428051db522a260ae4d39f6e8a608b87e6
github username <github email address>
2020-09-14 21:10:14 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code here!
+
+//      1) no longer has DOM node 'main#main'
+document.querySelector('main#main').remove();
+
+//      2) has a 'newHeader' variable that points to node 'h1#victory'
+const newHeader = document.createElement('h1');
+
+// has a 'newHeader' variable that points to node 'h1#victory'
+newHeader.id = 'victory';
+
+//      4) has a 'newHeader' variable that points to node 'h1#victory' with "YOUR-NAME is the champion" inside
+newHeader.innerHTML = 'Dahlia is the champion';
