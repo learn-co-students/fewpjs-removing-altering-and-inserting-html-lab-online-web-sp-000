92a7f7163cd3ba645fcdf91d0f6ed8895c5c2334
cantstopcoding <jonathan_koss@yahoo.com>
2020-10-01 10:35:19 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+main = document.querySelector("main#main");
+main.remove()
+
+let newHeader = document.createElement('h1')
+newHeader.id = "victory"
+newHeader.innerHTML = "Jonathan is the champion"
+document.body.appendChild(newHeader)
