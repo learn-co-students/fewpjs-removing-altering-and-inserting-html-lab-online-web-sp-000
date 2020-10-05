14c9a31cc5b9fae29fbfcb710d6090449e02ddc5
zchesny <z.chesny@gmail.com>
2020-10-04 18:31:36 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+// let mainEl = document.querySelector('main#main'); 
+// mainEl.remove(); 
+document.querySelector('main#main').remove(); 
+
+const newHeader = document.createElement('h1');
+newHeader.id = "victory" 
+newHeader.innerHTML = "Zoey is the champion"
+// document.body.appendChild(newHeader); 
