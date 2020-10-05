0536d3533a9a7d5cee89bda9b477461f0b43d265
rohanphillips <rohan@solutions-insurance.com>
2020-10-05 15:33:27 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+let element = document.getElementById('main');
+element.remove();
+
+let newHeader = document.createElement('h1')
+newHeader.setAttribute('id', 'victory')
+newHeader.innerHTML = "Rohan is the champion"
+document.body.appendChild(newHeader)
