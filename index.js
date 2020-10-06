1107f72b987553319a52c5f248e8687e71ae200b
bruffin1221 <bruffin1221@gmail.com>
2020-10-05 20:23:10 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+main.remove()
+
+let newHeader= document.querySelector("h1#victory")
+newHeader.innerHTML="Bruce is the champion. Make sure you create an <h1> with id 'victory'"
+
