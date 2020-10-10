9745fdd5ef1a99c7c3b8e496c2ce446dcdc74623
jessicagomesng <jgnpeluso@gmail.com>
2020-10-10 18:38:07 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code here!
+document.getElementById('main').remove();
+
+let newH = document.createElement('h1');
+newH.setAttribute('id', 'victory');
+let newHContent = document.createTextNode('YOUR-NAME is the champion');
+newH.appendChild(newHContent);
+const currentScript = document.body.querySelector('script');
+document.body.insertBefore(newH, currentScript);
+
+let newHeader = document.getElementById('victory')
+
+
