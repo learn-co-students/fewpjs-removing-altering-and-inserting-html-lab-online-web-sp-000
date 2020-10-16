b39f1bf6a82f32964fced6cfa8782c41d6a80375
Sergio Marelli <sergio.mrll@gmail.com>
2020-10-16 23:27:14 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+document.querySelector('main#main').remove()
+
+const newHeader = document.createElement('H1')
+newHeader.id = 'victory'
+
+document.body.appendChild(newHeader)
+
+newHeader.innerHTML = "me is the champion!";
