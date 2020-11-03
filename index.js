e49a3c18ed9895d9f54dcee75d1baabe95d40369
Taishawnk <Taishawnking1@yahoo.com>
2020-11-03 15:15:47 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Write your code here!
+let element = document.querySelector('#main');
+element.remove();
+
+let newHeader = document.createElement('h1');
+
+newHeader.setAttribute("id", "victory");
+
+newHeader.innerHTML = ' I is the champion';
+
+// document.body.appendChild('ul');
+
+// let ul = document.createElement('ul')
+
+// for (let i = 0; i < 3; i++) {
+//     let li = document.createElement('li')
+//     li.innerHTML = (i + 1).toString()
+//     ul.appendChild(li)
+// }
+
+// element.appendChild(ul)
