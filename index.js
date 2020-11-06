927acd9ac26a41746c72a27580368fd6e7221c2a
The-Gentleman <zraja4402@gmail.com>
2020-11-06 13:09:55 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code here!
+let element = document.querySelector("main");
+main.remove()
+
+let newHeader = document.createElement("h1")
+document.body.appendChild(newHeader)
+newHeader.id = "victory"
+newHeader.innerHTML = "is the champion"
