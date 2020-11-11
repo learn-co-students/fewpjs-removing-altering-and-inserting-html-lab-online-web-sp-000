b87b93cdeb6317ea92c7a2c9e46c8f064c3e53c2
no0ro <nrolwes@gmail.com>
2020-11-11 02:20:45 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+
+// no longer has DOM node 'main#main'
+main.remove()
+
+// has a 'newHeader' variable that points to node 'h1#victory'
+let newHeader = document.createElement('h1'); // create element
+newHeader.id = 'victory'; // add id to that element
+newHeader.innerHTML = "Noelle is the champion"; // add text 
+document.body.appendChild(newHeader) // append el to DOM node
