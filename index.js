1752c1a062759f1a356ed34bcbb849d5c6b10c67
ArslanInfinite <ali.arslan319@gmail.com>
2020-12-05 00:12:20 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+
+main.remove(); //remove the id with the name of 'main'
+
+let newHeader = document.createElement("h1"); //first create a valid CSS selector 
+
+newHeader.id = "victory" //then assign a id name to it
+
+newHeader.innerHTML = 'Arslan is the champion' //then add the innerHTML text to it
+
+document.appendChild(newHeader) //finally, this part adds it to the document itself
