e4b8d5edde969cd75577d4446f68c1727adfa93d
Ronda Cowen <cowenhome5@gmail.com>
2020-12-29 19:40:07 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+var el = document.getElementById('main');
+el.remove();
+const newHeader = document.createElement("h1");
+newHeader.id = "victory"
+newHeader.innerHTML = "Ronda is the champion!";
+newHeader.className = "victory";
