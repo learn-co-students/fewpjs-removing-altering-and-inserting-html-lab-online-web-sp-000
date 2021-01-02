c42d37da8b06747652f4d0f3655d02b111b464df
Drew Thompson <drewallynthompson@gmail.com>
2021-01-02 06:35:02 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+document.body.remove('main');
+
+let newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.innerHTML = 'YOUR-NAME is the champion';
+document.body.appendChild(newHeader);
