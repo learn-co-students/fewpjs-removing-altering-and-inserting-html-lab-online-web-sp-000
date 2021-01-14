765c9a0ba637e964b7bd229ecb5f14b915588c77
Alie Stone <helloaliestone@gmail.com>
2021-01-14 04:37:23 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+  main.remove();
+  var newHeader = document.createElement("h1");
+  newHeader.id = "victory";
+  newHeader.innerHTML = "Your name is the champion!";
+  document.body.appendChild(newHeader);
