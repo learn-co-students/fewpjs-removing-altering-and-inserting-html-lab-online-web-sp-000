7485885205d9e4015c2544b8f69ca3b17021eb33
jrog20 <jaimerogstad@gmail.com>
2021-01-25 20:31:49 -0500

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 main.remove()
 let newHeader = document.createElement('h1')
 newHeader.id = "victory"
-newHeader.innerHTML = 'J is the champion'
+newHeader.textContent = 'J is the champion'
 document.body.appendChild(newHeader)
