56be12cf0852622995cb00e095b16a45138ee5f6
khoinguyenkc <khoinguyenkc@gmail.com>
2021-02-01 00:01:31 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+let main = document.querySelector('#main');
+document.body.removeChild(main);
+
+let victory = document.createElement('h1');
+victory.id = 'victory';
+victory.innerHTML = 'YOUR-NAME is the champion';
+document.body.appendChild(victory);
+
+let newHeader = document.querySelector('h1#victory');
