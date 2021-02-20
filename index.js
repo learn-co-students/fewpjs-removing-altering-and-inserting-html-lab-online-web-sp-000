9f04b5c898348ca65e903b7039900f4670718c1b
nllew <n.lewis.perc@gmail.com>
2021-02-19 18:14:02 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+document.getElementById("main").remove();
+
+
+const newHeader = document.createElement('h1');
+newHeader.innerHTML = "Nathan is the champion"
+newHeader.id = "victory"
+
+
+
