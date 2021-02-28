d1c065711bb17f438fad92bc272c1ff896c19116
djenkala <daniel@jenkala.com>
2021-02-27 19:36:13 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+main.remove()
+
+let newHeader = document.createElement('h1');
+newHeader.setAttribute("id", "victory")
+newHeader.innerHTML = "Daniel is the champion"
+document.head.appendChild(newHeader)
