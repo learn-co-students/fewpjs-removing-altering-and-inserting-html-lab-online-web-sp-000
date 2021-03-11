3f64a907614262954b6c38dfbff064865affe0fb
Carrie Mathieu <carrieburks@gmail.com>
2021-03-11 17:16:14 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+let element = document.querySelector("main#main");
+element.remove();
+
+const newHeader = document.createElement('h1');
+newHeader.id = "victory"
+newHeader.innerHTML = "Carrie is the champion"
