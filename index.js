79db16b7134b90416939662b35fc6ee212ab13ec
RSigmaSG <sg.comparch@gmail.com>
2021-03-15 22:55:53 -0400

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 // Write your code here!
 main.remove();
-const newHeader = document.createElement('div');
+const newHeader = document.createElement('h1');
 newHeader.id = "victory";
-newHeader.classname = "victory";
 newHeader.innerHTML = "<h1>YOUR-NAME is the champion</h1>";
