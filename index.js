e76dd24336726753a217d883755d3a66c6b887a6
Nat McConnaughay <natmcconnaughay@gmail.com>
2021-03-17 18:28:06 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+let main = document.querySelector("main")
+main.remove()
+
+let newHeader = document.createElement("h1")
+newHeader.id = "victory"
+
+let text = document.createTextNode("Nat is the champion");
+
+newHeader.appendChild(text)
