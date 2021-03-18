df6985c31363215ed6f3a1a076f08b445dce4eb1
Sonja Parsell <hello@sparsell.com>
2021-03-18 07:51:51 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+
+// remove an entire element:
+main.remove();
+
+let newHeader = document.createElement("h1");
+newHeader.id = 'victory';
+newHeader.innerHTML = "Sonja is the champion"
