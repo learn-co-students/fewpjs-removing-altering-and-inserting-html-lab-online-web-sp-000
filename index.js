b35043f54cd742728d93b02d75919c35132a2afb
Ryan Burns <ryankburns@gmail.com>
2021-04-13 06:54:28 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+main.remove(main.querySelector('main#main'))
+
+let newHeader = document.createElement('h1');
+    document.body.appendChild(newHeader)
+    newHeader.id = 'victory'
+    newHeader.innerHTML = "Ryan is the champion"
