590988febc6b89e6d667d0b5c714c427142df2d5
daniel_feleppa <dfeleppa@gmail.com>
2021-04-13 19:56:26 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code here!
+// Write your code here!let element = document.querySelector("p#greeting");
+document.querySelector('main#main').remove();
+
+const newHeader = document.createElement('h1');
+
+newHeader.id = 'victory';
+newHeader.innerHTML = 'is the champion';
