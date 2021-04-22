0da5bbfb45435898475213fbf49e83d918d663d1
Keith Filkins <kafilkins@me.com>
2021-04-21 20:20:03 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+let main = document.getElementById("main")
+
+main.remove()
+
+let newHeader = document.createElement('h1');
+newHeader.id = "victory"
+newHeader.innerHTML = "Keith is the champion";
+newHeader.nodeName = "victory";
