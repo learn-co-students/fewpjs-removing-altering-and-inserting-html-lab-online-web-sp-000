84dbd911e967f408c94808c1555643a5d1e2f0e2
A.J. Romaniello <ajiellodev@gmail.com>
2021-04-30 11:20:57 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+document.getElementById('main').remove();
+
+let header = document.createElement('h1');
+header.id = "victory"
+header.innerHTML = 'AJ is the champion'
+document.body.appendChild(header)
+
+const newHeader = document.getElementById('victory')
