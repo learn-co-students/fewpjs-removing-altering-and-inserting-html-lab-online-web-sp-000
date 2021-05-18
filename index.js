56c591d4432f4a40b2bb8ccc631fa242908e3abe
jonathon McClendon <jonathonsmcclendon@gmail.com>
2021-05-18 16:58:04 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+
+document.getElementById("main").remove()
+let newHeader = document.createElement("h1")
+newHeader.innerHTML = "Jonathon is the champion"
+newHeader.id = "victory"
