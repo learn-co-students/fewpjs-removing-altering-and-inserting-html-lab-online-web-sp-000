2f1b5639163f5394702e23932cf23ec0797066a3
russell-macalintal <russell-macalintal@gmail.com>
2021-05-18 05:35:18 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+let mainElem = document.body.querySelector('main#main');
+mainElem.remove();
+
+let newHeader = document.createElement('h1');
+newHeader.setAttribute('id', 'victory');
+document.body.appendChild(newHeader);
+newHeader.innerHTML = "YOUR-NAME is the champion";
