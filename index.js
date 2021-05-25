bce609b5e17e7f8b440caf007fdc3bae590b15b8
amongiove <amandamongiove@gmail.com>
2021-05-25 15:57:21 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+main.remove()
+//-----------------------
+let element = document.createElement('H1')
+element.id = 'victory'
+let newHeader = document.body.appendChild(element)
+newHeader.innerHTML = "<h1>Amanda is the champion</h1>"
