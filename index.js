2c879bdc649efa5fd2e9a68de569c9f16e60acb3
Luis Martinez <luis_mmartinez@live.com>
2021-05-26 22:45:46 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+let element = document.getElementById('main')
+
+element.remove();
+
+let newHeader = document.createElement('h1');
+newHeader.id = "victory"
+newHeader.innerHTML= "X is the champion";
+
+// newHeader.innerHTML = "<h1 id = 'vitory'>X is the champion</h1>"
