6d0ac99508d649ea3d4b1d0dbb2e3275bfb8459a
Jacob Klimenko <droidedjake@gmail.com>
2021-06-18 01:19:20 -0400

    Done.

--- index.js
+++ index.js
@@ -4,3 +4,4 @@ document.body.removeChild(main);
 const newHeader = document.createElement("h1");
 newHeader.setAttribute("id", "victory");
 newHeader.innerHTML = "fancybandit is the champion";
+document.body.appendChild(newHeader);
